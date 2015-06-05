04e117ba159a91c95691d642fccf25dc71604411
tvdung94 <tran61@purdue.edu>
2015-06-06 00:23:17 +0700

    Tabhost added

--- app/src/main/java/com/org/dungtranvu/local_gaming_peers_finder/Feed_Fragment.java
+++ app/src/main/java/com/org/dungtranvu/local_gaming_peers_finder/Feed_Fragment.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
@@ -58,13 +59,31 @@ public class Feed_Fragment extends android.support.v4.app.Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed_, container, false);
+        View v = inflater.inflate(R.layout.fragment_feed_, container, false);
+        TabHost tb = (TabHost) v.findViewById(R.id.tabHost);
+        tb.setup();
+
+
+        TabHost.TabSpec ts = tb.newTabSpec("New");
+        ts.setContent(R.id.tab_new);
+        ts.setIndicator("New");
+        tb.addTab(ts);
+
+        TabHost.TabSpec ts2 = tb.newTabSpec("Hot");
+        ts2.setContent(R.id.tab_hot);
+        ts2.setIndicator("Hot");
+        tb.addTab(ts2);
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -84,7 +103,10 @@ public class Feed_Fragment extends android.support.v4.app.Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
+    @Override
+    public void onDestroyView() {
 
+    }
     @Override
     public void onDetach() {
         super.onDetach();
